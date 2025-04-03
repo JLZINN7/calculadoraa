a19373430322658e1cfec95b2a50c95786f35552
johnaiker <167939252+JLZINN7@users.noreply.github.com>
2025-04-03 19:19:19 -0300

    calcularoraa

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const display = document.getElementById('display');
+const botões = document.querySelectorAll('.botões button');
 
+let valorAtual = '';
+let valorAnterior = '';
+let operação = '';
+
+botões.forEach(botão => {
+ botão.addEventListener('click', () => {
+ const valorBotão = botão.textContent;
+
+ if (valorBotão === 'C') {
+ valorAtual = '';
+ valorAnterior = '';
+ operação = '';
+ display.value = '';
+ } else if (valorBotão === '=') {
+ calcular();
+ } else if (['+', '-', '*', '/'].includes(valorBotão)) {
+ operação = valorBotão;
+ valorAnterior = valorAtual;
+ valorAtual = '';
+ } else {
+ valorAtual += valorBotão;
+ display.value = valorAtual;
+ }
+ });
+});
+
+function calcular() {
+ let resultado;
+
+ switch (operação) {
+ case '+':
+ resultado = parseFloat(valorAnterior) + parseFloat(valorAtual);
+ break;
+ case '-':
+ resultado = parseFloat(valorAnterior) - parseFloat(valorAtual);
+ break;
+ case '*':
+ resultado = parseFloat(valorAnterior) * parseFloat(valorAtual);
+ break;
+ case '/':
